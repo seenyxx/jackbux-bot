25e8850e7c06b5405cb82b462f1a2cd72150fce2
seen-idc <seen.idc@gmail.com>
2021-12-28 22:58:54 +1100

    separated wallet

--- src/util/economy.ts
+++ src/util/economy.ts
@@ -3,19 +3,19 @@ import db from 'quick.db'
 const economy = new db.table('economy')
 
 export function addBalance(userId: string, added: number) {
-  economy.add(userId, added)
+  economy.add(`${userId}.wallet`, added)
 }
 
 export function subtractBalance(userId: string, subtracted: number) {
-  economy.subtract(userId, subtracted)
+  economy.subtract(`${userId}.wallet`, subtracted)
 }
 
 export function setBalance(userId: string, amount: number) {
-  economy.set(userId, amount)
+  economy.set(`${userId}.wallet`, amount)
 }
 
 export function getBalance(userId: string): number {
-  let bal = economy.get(userId)
+  let bal = economy.get(`${userId}.wallet`)
 
   if (!bal) {
     setBalance(userId, 50)
