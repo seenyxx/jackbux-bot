21e4b35abeeddaa0eeed49bbe7d0ed3df98e8f7c
seen-idc <seen.idc@gmail.com>
2022-01-01 21:25:25 +1100

    ok

--- src/commands/economy/damo.ts
+++ src/commands/economy/damo.ts
@@ -1,25 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { MessageEmbed, User } from 'discord.js'
 
 import { defCommand } from '../../util/commands'
 import { addBalanceNeutral } from '../../util/economy'
-import {
-  addBalance,
-  random,
-  getBalance,
-  subtractBalance,
-  numberRegex,
-  jackbuxEmoji,
-} from '../../util/economy'
-
-function giveCurrencyEmbed(user: User, amount: number, receivingUser: User) {
-  const embed = new MessageEmbed()
-    .setColor('RANDOM')
-    .setTitle(`**${user.tag}** just gave **${amount} ${jackbuxEmoji}** to ${receivingUser.tag}`)
-    .setDescription(`${receivingUser} has received \`${amount}\` ${jackbuxEmoji} from you.`)
-
-  return embed
-}
 
 export default defCommand({
   name: 'damo',
@@ -37,6 +19,6 @@ export default defCommand({
     return
   },
   slashCommand: new SlashCommandBuilder()
-    .setName('give')
-    .setDescription('Give someone else your JACKBUX'),
+    .setName('test')
+    .setDescription('test'),
 })
